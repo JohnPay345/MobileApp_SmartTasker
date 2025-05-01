b6a40564768362015bbeefa09d08fd7e2660a86c
Ainur Khalikov <aleshaserg05@mail.ru>
2025-05-01 13:11:55 +0500

    Теперь IP-адресом по умолчанию - 0.0.0.0

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import { routes } from "./routes/index.js";
 config();
 
 const port = process.env.PORT || 8080;
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || '0.0.0.0';
 const fastify = Fastify({
   logger: {
     transport: {
